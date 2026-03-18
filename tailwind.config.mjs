10e7230351ae10964af77d39bbfee39b351cd3bc
Agentive Concepts <tidutch531@gmail.com>
2026-03-18 22:11:44 +0100

    Update UI: Square corners & Folex Contact Page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    borderRadius: {
+      none: "0",
+      DEFAULT: "0",
+      sm: "0",
+      md: "0",
+      lg: "0",
+      xl: "0",
+      "2xl": "0",
+      "3xl": "0",
+      full: "0",
+    },
     extend: {
       colors: {
         white: "#FFFFFF",
